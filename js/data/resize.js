a5903127acf0552d47d3ae153bbd3d1bc704c1f3
Ashot <galsash@gmail.com>
2018-11-28 20:09:37 +0400

    fix pareseInt > Math.floor

--- js/data/resize.js
+++ js/data/resize.js
@@ -2,5 +2,5 @@ export const resize = (holder, original) => {
   const ratioWidth = original.width / holder.width;
   const ratioHeight = original.height / holder.height;
   const ratio = Math.max(ratioWidth, ratioHeight);
-  return {width: parseInt(original.width / ratio, 10), height: parseInt(original.height / ratio, 10)};
+  return {width: Math.floor(original.width / ratio), height: Math.floor(original.height / ratio)};
 };
